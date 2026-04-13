2865bb8e52be00800776697205c322a8f2eb1eca
aiinnovax <aiinnovax@gmail.com>
2026-04-13 20:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-# Configuration settings
+SEARCH_ENGINE_CODE = """
+<script async src="https://cse.google.com/cse.js?cx=c24f4f6e2ae2d42ce"></script>
+<div class="gcse-search"></div>
+"""
+
+API_ENDPOINT = "https://www.googleapis.com/customsearch/v1"
 API_KEY = "AIzaSyD7buH7nFOwwq2kcQF68Ucm7jRIbNKBpYk"
-SEARCH_ENGINE_ID = "<script async src="https://cse.google.com/cse.js?cx=06d962c38ed374c29">
-</script>
-<div class="gcse-search"></div>"
+SEARCH_ENGINE_ID = "c24f4f6e2ae2d42ce"
