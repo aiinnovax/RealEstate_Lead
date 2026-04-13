0f54f24f2af46876a077563e067b8ea897d520fd
aiinnovax <aiinnovax@gmail.com>
2026-04-13 20:35:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Configuration settings
 API_KEY = "AIzaSyD7buH7nFOwwq2kcQF68Ucm7jRIbNKBpYk"
-SEARCH_ENGINE_ID = "<<script async src="https://cse.google.com/cse.js?cx=06d962c38ed374c29">
+SEARCH_ENGINE_ID = "<script async src="https://cse.google.com/cse.js?cx=06d962c38ed374c29">
 </script>
-<div class="gcse-search"></div> "
+<div class="gcse-search"></div>"
